38949134fb01c415ded87d7f1ca2f32313e354f3
luisayala323 <luisayala323@gmail.com>
2023-02-17 18:36:33 +0000

    type size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,15 +10,27 @@ int main(void)
 int intType;
 float floatType;
 double doubleType;
-char charType;
+char charTyp;
 
-printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of a char: 1 byte(s)")
 
-printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of an int: 4 byte(s)")
 
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
+printf("Size of a long int: 4 byte(s)")
 
-printf("Size of char: %zu bytes\n", sizeof(charType));
+printf("Size of a long long int: 8 byte(s)")
+
+printf("Size of a float: 4 byte(s)")
+
+printf("Size of a char: 1 byte(s)")
+
+printf("Size of an int: 4 byte(s)")
+
+printf("Size of a long int: 8 byte(s)")
+
+printf("Size of a long long int: 8 byte(s)")
+
+printf("Size of a float: 4 byte(s)")
 
 return (0);
 }
