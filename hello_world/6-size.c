2e671e7cc9f0bdbf8cc741b5fe072c70e1e6e198
luisayala323 <luisayala323@gmail.com>
2023-02-17 18:24:58 +0000

    type size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - entry point "Write a C program that prints the size of various types
+ * main - entry point "Write a C program that prints the size of various types"
  * Return: 0
  */
 int main(void)
