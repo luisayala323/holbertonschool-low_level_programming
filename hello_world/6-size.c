007347795bf51b4b0599a24380735dbf49f48de1
luisayala323 <luisayala323@gmail.com>
2023-02-17 18:56:22 +0000

    type size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,9 +14,13 @@ long long int longlongintType;
 float floatType;
 
 printf("Size of a char: %zu bytes\n", sizeof(charType));
+
 printf("Size of an int: %zu bytes\n", sizeof(intType));
+
 printf("Size of a long int: %zu bytes\n", sizeof(longintTipe));
+
 printf("Size of a long long int: %zu bytes\n", sizeof(longlongType));
+
 printf("Size of a float: %zu bytes\n", sizeof(floatType));
 
 return (0);
