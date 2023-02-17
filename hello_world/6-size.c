553599a240ea57248ec86c3b21057cd275468353
luisayala323 <luisayala323@gmail.com>
2023-02-17 18:27:05 +0000

    type size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+
 /**
  * main - entry point "Write a C program that prints the size of various types"
  * Return: 0
  */
+
 int main(void)
 {
 printf("Size of int: %zu bytes\n", sizeof(intType));
+
 printf("Size of float: %zu bytes\n", sizeof(floatType));
+
 printf("Size of double: %zu bytes\n", sizeof(doubleType));
+
 printf("Size of char: %zu bytes\n", sizeof(charType));
+
 return (0);
 }
