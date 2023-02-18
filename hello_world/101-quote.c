902cfbdf69f39a3dcf1b0ffe8e075dc9e8c55250
luisayala323 <luisayala323@gmail.com>
2023-02-18 23:05:32 +0000

    101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 /**
  * main - entry" point  program that prints
- * Return: 0
+ * Return: 1
  */
 int main(void);
 {
 	FILE *fp;
-	char str[] = ("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	char str[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
 
 	fp = fopen("file.txt", "w");
 	fwrite(str, 1, sizeof(str), fp);
 	fclose(fp);
-	return (0);
+	return (1);
 }
