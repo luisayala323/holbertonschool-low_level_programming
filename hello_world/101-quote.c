45ae3675e14b7a2faa880ea2439128b47e5c0116
luisayala323 <luisayala323@gmail.com>
2023-02-19 00:04:25 +0000

    101q

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,7 +8,7 @@ int main(void)
 	FILE *fp;
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fp = fopen("file.txt", "w");
+	fp = fopen("101-quote.c", "w");
 	fwrite(str, 1, sizeof(str), fp);
 
 	fclose(fp);
