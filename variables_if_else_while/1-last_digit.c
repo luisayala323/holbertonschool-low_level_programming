c328c709976720e5241c38fb8136cc100c495cd6
luisayala323 <luisayala323@gmail.com>
2023-02-20 21:09:16 +0000

    print the last digit of the number stored in the variable n

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
         last = n % 10;
 
         if (last > 5)
-                printf("Last digit of %d is %d and is greater than 5\n", n, Last);
+                printf("Last digit of %d is %d and is greater than 5\n", n, last);
         else if (last == 0)
-                printf("Last digit of %d is %d and is 0\n", n, Last);
+                printf("Last digit of %d is %d and is 0\n", n, last);
         else if (last < 6)
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Last);
+                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
         return (0);
 }
