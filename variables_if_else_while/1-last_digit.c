52ca6fdcee38c78e5a62e5d01bf7ffae6c276ba7
luisayala323 <luisayala323@gmail.com>
2023-02-20 21:13:29 +0000

    print the last digit of the number stored in the variable n

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- * main - "print the last digit of the number stored in the variable n"
+ * main - print the last digit of the number stored in the variable n
  * Return: 0
- *
  */
+
 int main(void)
 {
         int n, last;
