8e1f5c893c8a3cf78961de148d41ff5f8d4c70c3
luisayala323 <luisayala323@gmail.com>
2023-02-21 12:22:35 +0000

    base 10 starting from 0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - rints all single digit numbers base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int nume;
+
+	for (nume = 0; nume < 10; nume++)
+		printf("%d", nume);
+	printf("\n");
+	return(0);
+}
