951128e55dcc43ec2a232f5c7c7d182259422028
luisayala323 <luisayala323@gmail.com>
2023-02-21 21:53:29 +0000

    prints all possible combinations of single-digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int n = 48;
+
+		while (n <= 57)
+
+		{
+			putchar(n);
+			if (n != 57)
+
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			n++;
+		}
+
+	putchar('\n');
+	return (0);
+}
