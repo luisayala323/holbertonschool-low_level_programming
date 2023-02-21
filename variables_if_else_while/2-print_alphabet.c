4a2c7669f7185d0a76e3fff2ea31e6762c60a0d5
luisayala323 <luisayala323@gmail.com>
2023-02-21 13:59:11 +0000

    alphabet in reverse

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	char alfa = 'a'
-		;
+
 	while (alfa <= 'z')
 
 	{
---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	for(i='Z'; i>='A'; i--)
+
+		{
+		printf("%c", i);
+		}
+		
+	putchar('\n');
+		return(0);
+}
+
+
+
