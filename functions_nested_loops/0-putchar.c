d29b1b39eb6baab939fc861436bb80e0a8f05291
luisayala323 <luisayala323@gmail.com>
2023-02-22 15:26:14 +0000

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	putchar('_')
+		putchar('p')
+		putchar('u')
+		putchar('t')
+		putchar('c')
+		putchar('h')
+		putchar('a')
+		putchar('r')
+		putchar('\n')
+
+	return (write(1, &c, 1));
+}
