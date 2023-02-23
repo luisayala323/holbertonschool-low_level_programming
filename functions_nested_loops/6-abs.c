fc41164cc71b31a5e7350e029778dae0a10af6f3
luisayala323 <luisayala323@gmail.com>
2023-02-23 14:44:24 +0000

    sakfdsf

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @s: absolute value
+ * Return: 0  success
+ */
+int _abs(int s)
+
+{
+	if (s > 0)
+		return (s);
+	else
+		return (s * -1);
+
+}
