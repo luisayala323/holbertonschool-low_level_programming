0cd6f5e5d32dcd9746422a82cc90bc4311979b88
luisayala323 <luisayala323@gmail.com>
2023-02-28 22:26:53 +0000

    sddsads

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - function that prints half of a string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i;
+	int index;
+	int n;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+	}
+	index = i - 1;
+	n = (index / 2) + 1;
+	for (; n <= index; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
