d1ad6d02e636190fdd2de2965ed9b61a86939a0e
luisayala323 <luisayala323@gmail.com>
2023-03-02 13:33:40 +0000

    sdfdsf

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if equal or 1 if not equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int comp = 0;
+
+		while (*s1 != '\0' && *s2 != '\0')
+	{
+			if (*s1 != *s2)
+			{
+				comp = 1;
+			}
+
+		s1++;
+		s2++;
+	}
+
+	if (comp == 0)
+
+		return (0);
+	else
+		return (1);
+}
