750185feef8f79deee5a8490386a6abca03122e8
luisayala323 <luisayala323@gmail.com>
2023-03-02 01:10:00 +0000

    sfdas

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,7 @@ char *_strncpy(char *dest, char *src, int n)
 		len++;
 
 	for (i = 0; src[i] && i < n; i++)
-		dest[1] = src[i];
+		dest[i] = src[i];
 
 	for (i = len; i < n; i++)
 		dest[i] = '\0';
