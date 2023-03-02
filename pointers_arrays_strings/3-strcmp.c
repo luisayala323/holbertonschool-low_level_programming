c536386c094ba0ce8e84d31fc3cdfedf498a506f
luisayala323 <luisayala323@gmail.com>
2023-03-02 17:55:25 +0000

    dsfdsfd

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,26 +3,17 @@
  * _strcmp - compare two strings
  * @s1: string 1
  * @s2: string 2
- * Return: 0 if equal or 1 if not equal
+ * Return: tell numbers od spaces
  */
 int _strcmp(char *s1, char *s2)
 {
-	int flag = 0;
+	int p = 0;
 
-		while (*s1 != '\0' && *s2 != '\0')
-	{
-			if (*s1 != *s2)
-			{
-				flag = 1;
-			}
+	while (*(s1 + p) == *(s2 + p) && *(s1 + p))
+		p++;
 
-		s1++;
-		s2++;
-	}
-
-	if (flag == 0)
-
-		return (0);
+	if (*(s2 + p))
+		return (*(s1 + p) - *(s2 + p));
 	else
-		return (1);
+		return (0);
 }
