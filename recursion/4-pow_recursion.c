5779b1d2c9819e1ddafd29c6c7857b4ca992f37d
luisayala323 <luisayala323@gmail.com>
2023-03-07 20:38:22 +0000

    sadsad

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,12 +3,24 @@
  * _pow_recursion - returns the value and raised the power
  * @x: value
  * @y: raised power
+ * Return: value of power
  */
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
-		return (1);
 	if (y < 0)
-		return(_pow_recursion(x, y + 1) / x );
-	return (_pow_recursion(x, y - 1) * x);
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	if(x == 0)
+	{
+		return (0);
+	}
+
+	return (x * _pow_recursion(x, y -1));
 }
