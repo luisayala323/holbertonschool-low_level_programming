32522f8907b7547e5113e129df14be009eff6a76
luisayala323 <luisayala323@gmail.com>
2023-03-18 03:04:55 +0000

    ABS

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
 
-#define ABS(x) (((x < 0) ? (-x) : (x))
+#define ABS(x) (((x) < 0) ? (-x) : (x))
 
 #endif
