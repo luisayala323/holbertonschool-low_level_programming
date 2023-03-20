299c2ed2bf712be25b4ca6901fd27784c61e01d0
luisayala323 <luisayala323@gmail.com>
2023-03-20 15:18:11 +0000

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - a dog
+ * @char*
+ * @float
+ * @char*
+ *
+ * Description: new type with following element
+ */
+
+struct dog
+{
+  char* name;
+  float age;
+  char* owner;
+};
