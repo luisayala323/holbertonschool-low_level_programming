dbb4bd08d2d89470ea7985a9e07d85e8d6c93c88
luisayala323 <luisayala323@gmail.com>
2023-03-25 02:55:12 +0000

    variable value

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - function
+ * @format: list of types
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0, h = 0;
+	char *s = NULL;
+
+	va_start(args, format);
+	while (format && format[h])
+		h++;
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (i != h - 1)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
